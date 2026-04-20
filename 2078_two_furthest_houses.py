bcbd3c8c264d3d73d4a85276ac5ed7dfc22f7429
Aryan Sharma <aryansharmaa285@gmail.com>
2026-04-20 08:01:07 +0530

    Add solution for 2078: Two Furthest Houses
    Implement optimal solution to find maximum distance between two houses of different colors.

---
+++ 2078_two_furthest_houses.py
@@ -0,0 +1,20 @@
+"""
+Optimal Approach
+Time Complexity: O(n)
+Space Complexity: O(1)
+"""
+
+class Solution:
+    def maxDistance(self, colors):
+        n = len(colors)
+
+        max_dist = 0
+
+        for i in range(n):
+            if colors[i] != colors[0]:
+                max_dist = max(max_dist, i)
+
+            if colors[i] != colors[-1]:
+                max_dist = max(max_dist, n - 1 - i)
+
+        return max_dist
